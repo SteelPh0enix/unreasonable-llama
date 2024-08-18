cc844aa794311eeff32dde13e9d4057c66e082b4
SteelPh0enix <wojciech_olech@hotmail.com>
2024-08-18 12:27:41 +0200

    Restructured the package

--- format_and_check_code.ps1
+++ format_and_check_code.ps1
@@ -1,10 +1,5 @@
-#!/bin/sh
-
-python_files=$(ls ./unreasonable_llama/*.py)
-echo "Checked files: $python_files"
-
-set -xeo pipefail
-
+$python_files = Get-ChildItem -Path ./unreasonable_llama/*.py
+echo "Checked files: ${python_files}"
 poetry run ruff format $python_files
 poetry run mypy --strict --pretty $python_files
 poetry run ruff check --fix $python_files
