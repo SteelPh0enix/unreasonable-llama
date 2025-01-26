037156a26fa4828bf66ab3e1315cec358289cb58
SteelPh0enix <wojciech_olech@hotmail.com>
2025-01-26 22:10:59 +0100

    Rewritten the dataclasses

--- tests/__main__.py
+++ tests/__main__.py
@@ -1,5 +1,7 @@
 import argparse
-import asyncio
+
+# import asyncio
+import sys
 from pprint import pprint
 
 import unreasonable_llama as llama
@@ -13,36 +15,36 @@ if server_is_alive:
     print("Server is alive!")
 else:
     print("Server is NOT alive!")
-    exit(1)
+    sys.exit(1)
 
 props = llama.props()
 pprint(props)
 
-models = llama.models()
-pprint(models)
-
-if args.test_completion:
-    example_small_completion = llama.LlamaCompletionRequest(
-        prompt="Here's a random fact:",
-        n_predict=100,
-    )
-
-    print("Requesting blocking completion for")
-    pprint(example_small_completion)
-    sync_completion = llama.complete(example_small_completion)
-    pprint(sync_completion)
-
-    async def perform_async_completion(request: llama.LlamaCompletionRequest):
-        async for chunk in llama.streamed_complete(request):
-            pprint(chunk)
-
-    print("Requesting asynchronous completion for")
-    pprint(example_small_completion)
-    asyncio.run(perform_async_completion(example_small_completion))
-
-text_to_tokenize = "Hi, this is a sample sentence that's gonna be tokenized!"
-print(f"Tokenizing '{text_to_tokenize}'")
-tokens = llama.tokenize(text_to_tokenize)
-print(f"Tokens: {tokens}")
-detokenized_text = llama.detokenize(tokens)
-print(f"Detokenized text: '{detokenized_text}'")
+# models = llama.models()
+# pprint(models)
+#
+# if args.test_completion:
+#     example_small_completion = llama.LlamaCompletionRequest(
+#         prompt="Here's a random fact:",
+#         n_predict=100,
+#     )
+#
+#     print("Requesting blocking completion for")
+#     pprint(example_small_completion)
+#     sync_completion = llama.complete(example_small_completion)
+#     pprint(sync_completion)
+#
+#     async def perform_async_completion(request: llama.LlamaCompletionRequest) -> None:
+#         async for chunk in llama.streamed_complete(request):
+#             pprint(chunk)
+#
+#     print("Requesting asynchronous completion for")
+#     pprint(example_small_completion)
+#     asyncio.run(perform_async_completion(example_small_completion))
+#
+# text_to_tokenize = "Hi, this is a sample sentence that's gonna be tokenized!"
+# print(f"Tokenizing '{text_to_tokenize}'")
+# tokens = llama.tokenize(text_to_tokenize)
+# print(f"Tokens: {tokens}")
+# detokenized_text = llama.detokenize(tokens)
+# print(f"Detokenized text: '{detokenized_text}'")
--- unreasonable_llama/__init__.py
+++ unreasonable_llama/__init__.py
@@ -32,28 +32,178 @@ This library uses `httpx`. In case of connection issues, expect
 
 from __future__ import annotations
 
-from dataclasses import dataclass
+import os
+from dataclasses import dataclass, field
+
+import httpx
+from dataclasses_json import Undefined, config, dataclass_json
 
 
+@dataclass_json(undefined=Undefined.RAISE)
 @dataclass
 class ModelInfo:
-    """
-    Model information, fetched via `/model` endpoint
-    Use `models()` to
-    """
-
     id: str
-    """Model's ID"""
     created: int
-    """Creation date"""
     vocab_type: str
-    """Vocabulary type"""
     n_vocab: int
-    """Size of the vocabulary"""
     n_ctx_train: int
-    """Size of the context the model was trained on"""
     n_embd: int
     n_params: int
-    """Amount of model's parameters"""
     size: int
-    """Model's size"""
+
+
+@dataclass_json(undefined=Undefined.RAISE)
+@dataclass
+class GenerationParams:
+    n_predict: int
+    seed: int
+    temperature: float
+    dynatemp_range: float
+    dynatemp_exponent: float
+    top_k: int
+    top_p: float
+    min_p: float
+    xtc_probability: float
+    xtc_threshold: float
+    typical_p: float
+    repeat_last_n: int
+    repeat_penalty: float
+    presence_penalty: float
+    frequency_penalty: float
+    dry_multiplier: float
+    dry_base: float
+    dry_allowed_length: int
+    dry_penalty_last_n: int
+    dry_sequence_breakers: list[str]
+    mirostat: int
+    mirostat_tau: float
+    mirostat_eta: float
+    stop: list[str]
+    max_tokens: int
+    n_keep: int
+    n_discard: int
+    ignore_eos: bool
+    stream: bool
+    logit_bias: list[int]
+    n_probs: int
+    min_keep: int
+    grammar: str
+    samplers: list[str]
+    speculative_n_max: int = field(metadata=config(field_name="speculative.n_max"))
+    speculative_n_min: int = field(metadata=config(field_name="speculative.n_min"))
+    speculative_p_min: float = field(metadata=config(field_name="speculative.p_min"))
+    timings_per_token: bool
+    post_sampling_probs: bool
+    lora: list[str]
+
+
+@dataclass_json(undefined=Undefined.RAISE)
+@dataclass
+class GenerationSettings:
+    id: int
+    id_task: int
+    n_ctx: int
+    speculative: bool
+    is_processing: bool
+    non_causal: bool
+    params: GenerationParams
+    prompt: str
+    next_token: NextToken
+
+
+@dataclass_json(undefined=Undefined.RAISE)
+@dataclass
+class NextToken:
+    has_next_token: bool
+    has_new_line: bool
+    n_remain: int
+    n_decoded: int
+    stopping_word: str
+
+
+@dataclass_json(undefined=Undefined.RAISE)
+@dataclass
+class ModelProps:
+    default_generation_settings: GenerationSettings
+    total_slots: int
+    model_path: str
+    chat_template: str
+    build_info: str
+
+
+@dataclass_json(undefined=Undefined.RAISE)
+@dataclass
+class Slot:
+    id: int
+    id_task: int
+    n_ctx: int
+    speculative: bool
+    is_processing: bool
+    non_casual: bool
+    params: GenerationParams
+    prompt: str
+    next_token: NextToken
+
+
+class LlamaError(RuntimeError):
+    """llama.cpp base exception, should be subclassed if more details
+    useful to the user can be provided."""
+
+    def __init__(self, details: str, *args: object) -> None:
+        self.details = details
+        super().__init__(*args)
+
+    def __str__(self) -> str:
+        return f"LlamaError: {self.details}"
+
+
+class LlamaUrlError(LlamaError):
+    """Exception thrown in case of invalid/unknown URL of llama.cpp server"""
+
+    def __init__(self, details: str, host: str | None, port: int | None, *args: object) -> None:
+        self.host = host
+        self.port = port
+        super().__init__(f"{details} ({host}:{port})", *args)
+
+
+def _make_llama_server_url(host: str | None, port: int | None) -> str:
+    """creates llama.cpp server URL out of host/port, if not provided - will
+    try to fetch them from environment. Validates the port range."""
+    if host is None:
+        if env_host := os.getenv("LLAMA_ARG_HOST"):
+            host = env_host
+        else:
+            raise LlamaUrlError("Unknown llama.cpp server host!", host, port)
+
+    if port is None:
+        if env_port := os.getenv("LLAMA_ARG_PORT"):
+            port = int(env_port)
+        else:
+            raise LlamaUrlError("Unknown llama.cpp server port!", host, port)
+
+    if port <= 0 or port > 65535:  # noqa: PLR2004
+        raise LlamaUrlError("Invalid llama.cpp server port!", host, port)
+
+    return f"http://{host}:{port}"
+
+
+def health(
+    server_host: str | None = None,
+    server_port: int | None = None,
+    timeout: float = 60.0,
+) -> bool:
+    """Returns `True` if server is alive and ready, `False` if it's not ready"""
+    server_url = _make_llama_server_url(server_host, server_port)
+    response = httpx.get(f"{server_url}/health", timeout=timeout).json()
+    return bool(response.get("status", "") == "ok")
+
+
+def props(
+    server_host: str | None = None,
+    server_port: int | None = None,
+    timeout: float = 60.0,
+) -> bool:
+    """Returns `True` if server is alive and ready, `False` if it's not ready"""
+    server_url = _make_llama_server_url(server_host, server_port)
+    response = httpx.get(f"{server_url}/props", timeout=timeout).read()
+    return ModelProps.from_json(response)
