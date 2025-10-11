8d17a12fd8c373c782fb7a380381ddd4b1fb7871
SteelPh0enix <wojciech_olech@hotmail.com>
2025-10-11 21:54:20 +0200

    Updated test script to add module path to it's PATH

--- tests/__main__.py
+++ tests/__main__.py
@@ -1,3 +1,31 @@
+import os
+import sys
+from pathlib import Path
+
+
+def set_env_var_if_unset(var_name: str, value: str) -> None:
+    if (env_value := os.getenv(var_name)) is None:
+        print(f"Setting {var_name} to {value}")
+        os.environ[var_name] = value
+    else:
+        print(f"Retaining {var_name} value: {env_value}")
+
+
+def add_dir_to_path(dir: Path) -> None:
+    if dir.is_dir():
+        dir_str = str(dir.absolute())
+        if dir_str not in sys.path:
+            sys.path.append(dir_str)
+            print(f"Added directory to PATH: {dir}")
+        else:
+            print(f"Directory already in PATH: {dir}")
+    else:
+        print(f"Warning: directory not found: {dir}")
+
+
+MODULE_DIR = Path(__file__).parent.parent.resolve()
+add_dir_to_path(MODULE_DIR)
+
 import argparse
 import asyncio
 from pprint import pprint
@@ -32,7 +60,7 @@ if args.test_completion:
     sync_completion = llama.complete(example_small_completion)
     pprint(sync_completion)
 
-    async def perform_async_completion(request: llama.LlamaCompletionRequest):
+    async def perform_async_completion(request: llama.LlamaCompletionRequest) -> None:
         async for chunk in llama.streamed_complete(request):
             pprint(chunk)
 
